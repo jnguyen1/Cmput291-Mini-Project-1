57b56f2f84dbc2d159d7e0709ad1817fc13b345a
John Nguyen <jnguyen1@um11.cs.ualberta.ca>
2010-03-08 14:39:27 -0700

    Moved query 1 to main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.sql.*;
 import java.io.*;
 import java.util.Vector;
+import java.util.Collections;
 
 public class Main
 {
@@ -35,5 +36,54 @@ public class Main
 		{
 			System.out.println("SQL error in connecting to database.");
 		}
+	}
+
+	/*
+	 * Function:
+	 * Searches through pages table for records whose title or content contains the supplied keywords.
+	 * Display the results in a sorted order that places greater weighting on matches on title (T*2+C).
+	 *
+	 * Param:
+	 * conn - the connection to the database.
+	 * keywords - a vector of 1 or more keywords to match on.
+	 *
+	 * Return:
+	 * None.
+	 *
+	 * jnguyen1 20100307
+	 */
+	private void searchPages(Connection conn, Vector<String> keywords) throws SQLException
+	{
+		Vector<SearchPageObject> results = new Vector<SearchPageObject>();
+
+		String condition = "title like '" + keywords.get(0) + "' or content like '" + keywords.get(0) + "'";
+		for (int i=1; i<keywords.size(); i++)
+		{
+			condition.concat(" or title like '" + keywords.get(i) + "' or content like '" + keywords.get(i) + "'");
+		}
+
+		Statement stmt = conn.createStatement(); 
+		ResultSet rset = stmt.executeQuery("select * from pages where " + condition + ";"); 
+
+		while(rset.next())
+		{ 
+			SearchPageObject spo = new SearchPageObject(
+				rset.getString("pid"),
+				rset.getDate("cdate"),
+				rset.getString("title"),
+				rset.getString("content"),
+				rset.getString("creator")
+				);
+			spo.calculateRanking(keywords);
+
+			results.add(spo);
+		} 
+
+		Collections.sort(results);
+		/*
+		 * Do somethign with the sorted result. Probably print it to user in sorted order.
+		 */
+
+		stmt.close(); 
 	}
 }
