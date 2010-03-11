b292017c5629f15692a11323c9ecaa7ec86ddad0
John <ipwnponies@ipwnponies.(none)>
2010-03-11 03:13:08 -0700

    Mostly formatting.

--- Main.java
+++ Main.java
@@ -2,24 +2,35 @@ import java.sql.*;
 
 public class Main {
 
-	static String m_url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
-	static String m_driverName = "oracle.jdbc.driver.OracleDriver";
+	private static String m_url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+	private static String m_driverName = "oracle.jdbc.driver.OracleDriver";
 
-	static String m_userName = "schwehr";
-	static String m_password = "urarurar12";
+	private static String m_userName = "schwehr";
+	private static String m_password = "urarurar12";
 
-	static Connection m_con;
-	static Statement stmt;
+	private static Connection m_con;
+	private static Statement stmt;
 
-	static String user;
+	private static String user;
 
-	public static void main(String[] args) {
+	/**
+	 * Function:
+	 * Main function.
+	 *
+	 * Param:
+	 * args - an array of the commandline arguments.
+	 *
+	 * Return:
+	 * None.
+	 *
+	 * jnguyen1 20100310
+	 */
+	public static void main(String[] args)
+	{
 		try
 		{
-
 			Class drvClass = Class.forName(m_driverName);
-			DriverManager.registerDriver((Driver)
-					drvClass.newInstance());
+			DriverManager.registerDriver( (Driver)drvClass.newInstance() );
 			m_con = DriverManager.getConnection(m_url, m_userName, m_password);
 			stmt = m_con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 
@@ -30,7 +41,8 @@ public class Main {
 			int input;
 			boolean menu = true;
 
-			while(menu){
+			while(menu)
+			{
 				System.out.println();
 				System.out.println("Main menu:");
 				System.out.println("1. Login (for registered users)");
@@ -38,11 +50,13 @@ public class Main {
 				System.out.println("3. Exit");
 				input = Keyboard.in.readInteger().intValue();
 
-				switch(input){
+				switch(input)
+				{
 					case 1:
-						if(login(stmt)){
+						if (login(stmt, Main.user))
+						{
 							System.out.println("Login successful... Please wait to be directed to the user screen.");
-							UserPage nUser = new UserPage(m_con, user);
+							UserPage nUser = new UserPage(m_con, Main.user);
 							nUser.startUp();
 						}
 						else
@@ -61,10 +75,9 @@ public class Main {
 			}
 
 			System.out.println("Goodbye!");
-
-		} catch(Exception e)
+		}
+		catch(Exception e)
 		{
-
 			System.err.print("ClassNotFoundException: ");
 			System.err.println(e.getMessage());
 
@@ -83,8 +96,21 @@ public class Main {
 		}
 	}
 
-	public static boolean login(Statement stmt){
-
+	/**
+	 * Function:
+	 * Attempt to login the user.
+	 *
+	 * Param:
+	 * stmt - Statement object to run select.
+	 * user - the name of the account to login to.
+	 *
+	 * Return:
+	 * True if the credentials are correct.
+	 *
+	 * schwehr 20100310
+	 */
+	private static boolean login(Statement stmt, String user)
+	{
 		String pass;
 
 		System.out.print("E-mail: ");
@@ -93,27 +119,40 @@ public class Main {
 		System.out.print("Password: ");
 		pass = Keyboard.in.readString();
 
-		String createString = "select email, pwd from users where email ='"+user+"' and pwd ='"+pass+"'";
-
 		try
 		{
+			String query = String.format("select email, pwd from users where email ='%s' and pwd ='%s'", user, pass);
+			ResultSet rset = stmt.executeQuery(query);		
 
-			ResultSet rs = stmt.executeQuery(createString);		
-
-			if(rs.next())
+			// There should only be one record found if the credentials are valid..
+			rset.last();
+			if (rs.getRow() == 1)
+			{
 				return true;
-
-		} catch(SQLException ex) {
-
-			System.err.println("SQLException: " +
-					ex.getMessage());
-
+			}
+		}
+		catch(SQLException ex)
+		{
+			System.err.println("SQLException: " + ex.getMessage());
 		}
 
 		return false;
 	}
 
-	public static void register(Statement stmt){
+	/**
+	 * Function:
+	 * Register a new user.
+	 *
+	 * Param:
+	 * stmt - Statement object to run select and insert query.
+	 *
+	 * Return:
+	 * None.
+	 *
+	 * schwehr 20100310 
+	 */
+	private static void register(Statement stmt)
+	{
 		String email, name, city, pass, pass2; 
 		char[] gender;
 
@@ -132,7 +171,7 @@ public class Main {
 		{
 			System.out.print("Please enter either M or F for gender: ");
 			gender = Keyboard.in.readString().toLowerCase().toCharArray();
-		} while( !(gender.length == 1 && (gender[0] == 'm' || gender[0] == 'f')));
+		} while ( !(gender.length == 1 && (gender[0] == 'm' || gender[0] == 'f')) );
 
 		System.out.print("Finally, enter your password: ");
 		pass = Keyboard.in.readString();
@@ -140,7 +179,8 @@ public class Main {
 		System.out.print("Confirm your password: ");
 		pass2 = Keyboard.in.readString();
 
-		while(pass.compareTo(pass2) != 0){
+		while (pass.compareTo(pass2) != 0)
+		{
 			System.out.println("Passwords do not match!");
 
 			System.out.print("Please re-enter your password: ");
@@ -152,18 +192,17 @@ public class Main {
 
 		System.out.println("Thank you. Entering information into database... Please wait a moment.");
 
-		String createString = "insert into users values ('"+email+"','"+name+"','"+city+"','"+gender[0]+"','"+pass+"')";
-
-		try{
-			stmt.executeUpdate(createString);
+		try
+		{
+			String query = String.format("insert into users values ('%s','%s','%s','%c','%s')", email, name, city, gender[0], pass);
+			stmt.executeUpdate(query);
 
 			System.out.println("Congrats! You have created your account. You will now be directed to the login screen.");
 			Keyboard.in.pause();
-		} catch(SQLException ex) {
-
-			System.err.println("SQLException: " +
-					ex.getMessage());
-
+		}
+		catch (SQLException ex)
+		{
+			System.err.println("SQLException: " + ex.getMessage());
 		}
 	}
 
